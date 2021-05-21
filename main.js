1aeaf120aab03938065f56eae9141cb52a6806c7
greatmjh <66207287+greatmjh@users.noreply.github.com>
2021-05-21 08:46:53 +0200

    Make year text update automatically
    Make it so that if you keep the page open over the 31st of December, the year changes at midnight

--- main.js
+++ main.js
@@ -5,24 +5,35 @@ function yearToTimestamp(year){
 
 function update() {
     year = new Date().getFullYear(); //Get current year
+    
+    //Set year in page
+    for (i = 0; i < document.getElementsByClassName("year").length; i++) {
+        document.getElementsByClassName("year")[i].innerHTML = year;
+    }
+    
+    //Get progress through the year
     yearStart = yearToTimestamp(year); //Unix time of beginning of current year
     nextYearStart = yearToTimestamp(year + 1); //Unix time of beginning of next year
     yearLength = nextYearStart - yearStart; //Length of the year in miliseconds
-    now = Math.round(Date.now()); //Unix time noe
+    now = Math.round(Date.now()); //Unix time now
     timeElapsed = now - yearStart; //Unix time between now and the beginning of the year
     progress = timeElapsed / yearLength; //Progress through year as float
+    
     //Convert float to percentage rounded to 7 decimal places
     progress = progress * 1000000000;
     progress = Math.round(progress);
     progress = progress / 10000000;
+    
     //Display in progress bar
     document.getElementById("year-prog").value = progress * 100;
+    
     //Pad extra zeroes if necessary
     progress = String(progress);
     if (progress.length == 2) {
         //No decimal
         progress = progress + ".0000000";
     } 
+    
     //Pad the extra zeroes
     while (progress.length <= 9) {
         progress = progress + "0";
@@ -35,9 +46,7 @@ function update() {
 function main() {
     //Set year
     year = new Date().getFullYear();
-    for (i = 0; i < document.getElementsByClassName("year").length; i++) {
-        document.getElementsByClassName("year")[i].innerHTML = year;
-    }
+    
     update()
     setInterval(update, 10);
 }
